ca5e7b24a9a14f15c820d27cb8d3acf0493096bf
Austin Gallup <agallup@pdx.edu>
2022-02-18 02:18:56 -0800

    set up

---
+++ asteroids.cpp
@@ -0,0 +1,21 @@
+/***************************************************
+ * Author: Austin Gallup
+ * Date: 2/18/22
+ * File: asteroids.cpp
+ * Description: asteroids.cpp is the main game routine for the game asteroids.
+ * asteroids.cpp contains the game loop and all basic structure to run the game. 
+ * **************************************************/
+
+
+int 
+main()
+{
+    bool run_game = 1;
+
+    while(run_game)
+    {
+       
+    }
+
+    return 0;
+}
